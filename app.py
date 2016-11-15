794ba82241ff751d26254f1afd468cfbb7dd5b14
Grant Bachman <gbachman@rentjungle.com>
2016-11-15 22:39:56 +0000

    Respond only to github issue events

--- app.py
+++ app.py
@@ -4,14 +4,22 @@ from models import GitHubEvent
 
 app = Flask(__name__)
 
+
 @app.route('/github/hook', methods=['POST'])
 def github_issue():
     event = request.get_json()
 
     g = GitHubEvent(event)
+
+    # only handle issue events for now
+    if request.headers.get('X-GitHub-Event') != 'issues':
+        pass
+
     if event['action'] == 'opened':
         g.handle_opened_issue()
     return "Thanks."
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=4567)
